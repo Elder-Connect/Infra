22c654dada81ade909837b3ba06f54bc2f2c6f2a
Murilo Almeida <murilo.leite@sptech.school>
2024-05-21 11:36:04 -0300

    @MurilAlmeid alteração no script de create

--- Script Banco.sql
+++ Script Banco.sql
@@ -15,24 +15,24 @@ CREATE TABLE users(
 	document VARCHAR(18) UNIQUE NOT NULL,
 	birth_date TIMESTAMP NULL,
 	biography VARCHAR(511) NULL,
-	profile_picture VARCHAR (511 NULL),
-	user_type_id SERIAL, FOREIGN KEY (user_type_id) REFERENCES user_types(id),
-	gender_id SERIAL NULL, FOREIGN KEY (gender_id) REFERENCES genders(id)
+	profile_picture VARCHAR (511) NULL,
+	user_type_id INTEGER, FOREIGN KEY (user_type_id) REFERENCES user_types(id),
+	gender_id INTEGER NULL, FOREIGN KEY (gender_id) REFERENCES genders(id)
 );
 
 CREATE TABLE calendars(
 	id SERIAL PRIMARY KEY,
 	calendar_id VARCHAR(255),
 	type VARCHAR(45),
-	user_id SERIAL, FOREIGN KEY (user_id) REFERENCES users(id)
+	user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE messages(
 	id SERIAL PRIMARY KEY,
 	content VARCHAR(511),
 	date_time TIMESTAMP,
-	recipient_id SERIAL, FOREIGN KEY (recipient_id) REFERENCES users(id),
-	sender_id SERIAL, FOREIGN KEY (sender_id) REFERENCES users(id)
+	recipient_id INTEGER, FOREIGN KEY (recipient_id) REFERENCES users(id),
+	sender_id INTEGER, FOREIGN KEY (sender_id) REFERENCES users(id)
 );
 
 CREATE TABLE proposals(
@@ -42,14 +42,14 @@ CREATE TABLE proposals(
 	day_start_time TIMESTAMP,
 	day_time_end TIMESTAMP,
 	price NUMERIC(10,2),
-	recipient_id SERIAL, FOREIGN KEY (recipient_id) REFERENCES users(id),
-	sender_id SERIAL, FOREIGN KEY (sender_id) REFERENCES users(id)
+	recipient_id INTEGER, FOREIGN KEY (recipient_id) REFERENCES users(id),
+	sender_id INTEGER, FOREIGN KEY (sender_id) REFERENCES users(id)
 );
 
 CREATE TABLE ratings(
 	id SERIAL PRIMARY KEY,
 	rating INT,
-	proposal_id SERIAL, FOREIGN KEY (proposal_id) REFERENCES proposals(id)
+	proposal_id INTEGER, FOREIGN KEY (proposal_id) REFERENCES proposals(id)
 );
 
 CREATE TABLE specialties(
@@ -60,23 +60,23 @@ CREATE TABLE specialties(
 CREATE TABLE resumes(
 	id SERIAL PRIMARY KEY,
 	user_id SERIAL, FOREIGN KEY (user_id) REFERENCES users(id),
-	specialtie_id SERIAL NULL, FOREIGN KEY (specialtie_id) REFERENCES specialties(id) 
+	specialtie_id INTEGER NULL, FOREIGN KEY (specialtie_id) REFERENCES specialties(id) 
 );
 
 CREATE TABLE adresses(
 	id SERIAL PRIMARY KEY,
 	zip_code VARCHAR(10),
 	street VARCHAR(255),
-	complement VARCHAR(10),
+	complement VARCHAR(10) NULL,
 	neighborhood VARCHAR(225),
-	number VARCHAR(10),
+	number VARCHAR(10) NULL,
 	city VARCHAR(25),
 	uf VARCHAR(4)
 );
 
 CREATE TABLE residences(
 	id SERIAL,
-	user_id SERIAL, FOREIGN KEY (user_id) REFERENCES users(id),
-	adresse_id SERIAL, FOREIGN KEY (adresse_id) REFERENCES adresses(id),
+	user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id),
+	adresse_id INTEGER, FOREIGN KEY (adresse_id) REFERENCES adresses(id),
 	PRIMARY KEY (id, user_id, adresse_id)
 );
