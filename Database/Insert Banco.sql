32ac7c05f50ddd464b5ca7224c8dafe1738cdaa9
DaniloPedrazzi <danilo.justino1@gmail.com>
2024-11-20 15:01:31 -0300

    Migrate documentation to https://github.com/Elder-ly/Documentation

--- Database/Insert Banco.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO elder_ly.tb_genders (name) values
-('Masculino'),
-('Feminino'),
-('Prefiro não informar');
-
-INSERT INTO elder_ly.tb_user_types (name) values
-('ADM'),
-('Colaborador'),
-('Cliente');
